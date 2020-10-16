5655cd9024b17c9e0fb806283298f31ef10c1bd7
joinThe <GITHUB stadnikgleb@gmail.com>
2020-10-16 05:13:54 +0300

    -add contact me bytton and area

--- src/components/body/Contact.js
+++ src/components/body/Contact.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import FontIcon from "./FontIcon";
+
+let textArea = React.createRef();
+
+let click = ()=> {
+    alert(textArea.current.value);
+};
 
 function Contact() {
     return (
 
         <section className="contact">
             <div className="container">
-            <h2 className="title">My contacts</h2>
-            <div className="contact__phones">
-                <span className="phone">
-                    80*******
-                </span>
+            <div className="phones">
+                        <i className="fas fa-mobile-alt"></i>
+                        <div className="phone-grope">
+
+                            <p className="phone">+380997374076<FontIcon name='telegram'/>
+                            </p>
+                            <p className="phone">+380638238310<FontIcon name='viber'/>
+                            </p>
+
+                        </div>
+
+                    </div>
+            <div class="email">
+                <span>stadnikGleb@gmail.com</span>
             </div>
+            <div class="text-for-me">
+                <h3 className="title">You also can write me: </h3>
+                
+                    <textarea name="text" id="textForMe" ref={textArea} className="area"></textarea>
+
+                    <button onClick={click} className="link-and-button"> Send</button>
+
+            </div>
+
             </div>
         </section>
     );
